29e5892f886641c40ce3dc0373f5be6cadbc107b
silvio <silviocesar.silva2903@gmail.com>
2024-04-28 16:14:34 -0300

    pasta com o codigo fonte

---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+
+class UsersTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('users');
+        $this->setDisplayField('nome');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('nome')
+            ->maxLength('nome', 25)
+            ->requirePresence('nome', 'create')
+            ->notEmptyString('nome');
+
+        $validator
+            ->decimal('idade')
+            ->requirePresence('idade', 'create')
+            ->notEmptyString('idade');
+
+        return $validator;
+    }
+}
