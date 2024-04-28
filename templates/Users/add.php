c07190a07097fa2cd5239b4580ed5d2dbfb08575
silvio <silviocesar.silva2903@gmail.com>
2024-04-28 16:15:34 -0300

    pasta templates

---
+++ templates/Users/add.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \Cake\Datasource\EntityInterface $user
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="users form content">
+            <?= $this->Form->create($user) ?>
+            <fieldset>
+                <legend><?= __('Add User') ?></legend>
+                <?php
+                    echo $this->Form->control('nome');
+                    echo $this->Form->control('idade');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
---
+++ templates/Users/view.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \Cake\Datasource\EntityInterface $user
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit User'), ['action' => 'edit', $user->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete User'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="users view content">
+            <h3><?= h($user->nome) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Nome') ?></th>
+                    <td><?= h($user->nome) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($user->id) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Idade') ?></th>
+                    <td><?= $user->idade === null ? '' : $this->Number->format($user->idade) ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
